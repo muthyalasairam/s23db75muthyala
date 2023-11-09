6abd925488fc10aa9fd4af2e17f0bb5c108e0625
Sairam Muthyala <s567075@nwmissouri.edu>
2023-11-08 21:14:44 -0600

    modified post

--- controllers/entertainment.js
+++ controllers/entertainment.js
@@ -1,16 +1,13 @@
 var entertainment = require('../models/entertainment');
 // List of all Costumes
-exports.entertainment_List = function(req, res) {
+/*exports.entertainment_List = function(req, res) {
 res.send('NOT IMPLEMENTED: Entertainment list');
-};
+};*/
 // for a specific Costume.
 exports.entertainment_detail = function(req, res) {
 res.send('NOT IMPLEMENTED: Entertainment detail: ' + req.params.id);
 };
-// Handle Costume create on POST.
-exports.entertainment_create_post = function(req, res) {
-res.send('NOT IMPLEMENTED: Entertainment create POST');
-};
+
 // Handle Costume delete form on DELETE.
 exports.entertainment_delete = function(req, res) {
 res.send('NOT IMPLEMENTED: Entertainment delete DELETE ' + req.params.id);
@@ -20,6 +17,26 @@ exports.entertainment_update_put = function(req, res) {
 res.send('NOT IMPLEMENTED: Entertainment update PUT' + req.params.id);
 };
 
+exports.entertainment_create_post = async function(req, res) {
+    console.log(req.body)
+    let document = new entertainment();
+    // We are looking for a body, since POST does not have query parameters.
+    // Even though bodies can be in many different formats, we will be picky
+    // and require that it be a json object
+    // {"costume_type":"goat", "cost":12, "size":"large"}
+    document.Movies = req.body.Movies;
+    document.Genre = req.body.Genre;
+    document.Budget = req.body.Budget;
+    try{
+    let result = await document.save();
+    res.send(result);
+    }
+    catch(err){
+    res.status(500);
+    res.send(`{"error": ${err}}`);
+    }
+    };
+    
 
 exports.entertainment_List = async function(req, res) {
     try{
@@ -32,3 +49,15 @@ exports.entertainment_List = async function(req, res) {
     }
     };
     
+    exports.entertainment_view_all_Page = async function(req, res) {
+        try{
+        entertainment = await entertainment.find();
+        res.render('entertainment', { title: 'Entertainment Search Results', results: entertainment });
+        }
+        catch(err){
+        res.status(500);
+        res.send(`{"error": ${err}}`);
+        }
+        };
+       
+        
--- routes/entertainment.js
+++ routes/entertainment.js
@@ -1,9 +1,9 @@
 var express = require('express');
+const entertainment_controlers= require('../controllers/entertainment');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('entertainment', { title: 'Search Results Entertainment' });
-});
 
+router.get('/', entertainment_controlers.entertainment_view_all_Page);
 module.exports = router;
+
