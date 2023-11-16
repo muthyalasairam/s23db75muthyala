97df0b8a8dcfe01efd4d33aaa30640556befd266
Sairam Muthyala <s567075@nwmissouri.edu>
2023-11-15 22:23:19 -0600

    added detail and create

--- controllers/entertainment.js
+++ controllers/entertainment.js
@@ -1,46 +1,46 @@
 
 var Entertainment = require('../models/entertainment');
 // List of all Costumes
-exports.entertainment_list = function(req, res) {
-res.send('NOT IMPLEMENTED: Entertainment list');
+exports.entertainment_list = function (req, res) {
+    res.send('NOT IMPLEMENTED: Entertainment list');
 };
 // Handle Costume create on POST
 // Handle Costume delete form on DELETE.
-exports.entertainment_delete = function(req, res) {
-res.send('NOT IMPLEMENTED: Entertainment delete DELETE ' + req.params.id);
+exports.entertainment_delete = function (req, res) {
+    res.send('NOT IMPLEMENTED: Entertainment delete DELETE ' + req.params.id);
 };
 
 var theEntertainments
 // List of all Costumes
-exports.entertainment_list = async function(req, res) {
-try{
-theEntertainments = await Entertainment.find();
-console.log('theEntertainments',theEntertainments);
-res.send(theEntertainments);
-}
-catch(err){
-res.status(500);
-console.log(`theEntertainments ${err}}`);
-res.send(`{"error": ${err}}`);
-}
+exports.entertainment_list = async function (req, res) {
+    try {
+        theEntertainments = await Entertainment.find();
+        console.log('theEntertainments', theEntertainments);
+        res.send(theEntertainments);
+    }
+    catch (err) {
+        res.status(500);
+        console.log(`theEntertainments ${err}}`);
+        res.send(`{"error": ${err}}`);
+    }
 };
 
 //VIEWS
 //Handle a show all view
-exports.entertainment_view_all_Page = async function(req, res) {
-try{
-theEntertainments = await Entertainment.find();
-console.log('theEntertainments',theEntertainments);
-res.render('entertainment', { title: 'Entertainment Search Results', results: theEntertainments });
-}
-catch(err){
-res.status(500);
-console.log(`theEntertainments ${err}}`);
-res.send(`{"error": ${err}}`);
-}
+exports.entertainment_view_all_Page = async function (req, res) {
+    try {
+        theEntertainments = await Entertainment.find();
+        console.log('theEntertainments', theEntertainments);
+        res.render('entertainment', { title: 'Entertainment Search Results', results: theEntertainments });
+    }
+    catch (err) {
+        res.status(500);
+        console.log(`theEntertainments ${err}}`);
+        res.send(`{"error": ${err}}`);
+    }
 };
 
-exports.entertainment_create_post = async function(req, res) {
+exports.entertainment_create_post = async function (req, res) {
     console.log(req.body)
     let document = new Entertainment();
     // We are looking for a body, since POST does not have query parameters.
@@ -50,41 +50,79 @@ exports.entertainment_create_post = async function(req, res) {
     document.Movies = req.body.Movies;
     document.Genre = req.body.Genre;
     document.Budget = req.body.Budget;
+    try {
+        let result = await document.save();
+        res.send(result);
+    }
+    catch (err) {
+        res.status(500);
+        res.send(`{"error": ${err}}`);
+    }
+};
+
+exports.entertainment_detail = async function (req, res) {
+    console.log("detail" + req.params.id)
+    try {
+        result = await Entertainment.findById(req.params.id)
+        res.send(result)
+    } catch (error) {
+        res.status(500)
+        res.send(`{"error": document for id ${req.params.id} not found`);
+    }
+};
+
+exports.entertainment_update_put = async function (req, res) {
+    console.log(`update on id ${req.params.id} with body
+            ${JSON.stringify(req.body)}`)
+    try {
+        let toUpdate = await Entertainment.findById(req.params.id)
+        // Do updates of properties
+        if (req.body.Movies)
+            toUpdate.Movies = req.body.Movies;
+        if (req.body.Genre) toUpdate.Genre = req.body.Genre;
+        if (req.body.Budget) toUpdate.Budget = req.body.Budget;
+        let result = await toUpdate.save();
+        console.log("Sucess " + result)
+        res.send(result)
+    } catch (err) {
+        res.status(500)
+        res.send(`{"error": ${err}: Update for id ${req.params.id}
+            failed`);
+    }
+};
+
+exports.entertainment_delete = async function(req, res) {
+console.log("delete " + req.params.id)
+try {
+result = await Entertainment.findByIdAndDelete( req.params.id)
+console.log("Removed " + result)
+res.send(result)
+} catch (err) {
+res.status(500)
+res.send(`{"error": Error deleting ${err}}`);
+}
+};
+exports.entertainment_view_one_Page = async function(req, res) {
+    console.log("single view for id " + req.query.id)
     try{
-    let result = await document.save();
-    res.send(result);
+    result = await Entertainment.findById( req.query.id)
+    res.render('entertainmentdetail', 
+   { title: 'Entertainment Detail', toShow: result });
     }
     catch(err){
-    res.status(500);
-    res.send(`{"error": ${err}}`);
+    res.status(500)
+    res.send(`{'error': '${err}'}`);
+    }
+   };
+exports.entertainment_create_Page = function(req, res) {
+    console.log("create view")
+    try{
+    res.render('entertainmentcreate', { title: 'Entertainment Create'});
+    }
+    catch(err){
+    res.status(500)
+    res.send(`{'error': '${err}'}`);
     }
     };
     
-    exports.entertainment_detail = async function(req, res) {
-        console.log("detail" + req.params.id)
-        try {
-        result = await Entertainment.findById( req.params.id)
-        res.send(result)
-        } catch (error) {
-        res.status(500)
-        res.send(`{"error": document for id ${req.params.id} not found`);
-        }
-        };
-        
-        exports.entertainment_update_put = async function(req, res) {
-            console.log(`update on id ${req.params.id} with body
-            ${JSON.stringify(req.body)}`)
-            try {
-            let toUpdate = await Entertainment.findById( req.params.id)
-            // Do updates of properties
-            if(req.body.Movies)
-            toUpdate.Movies = req.body.Movies;
-            if(req.body.Genre) toUpdate.Genre = req.body.Genre;
-            if(req.body.Budget) toUpdate.Budget = req.body.Budget;
-            let result = await toUpdate.save();
-            console.log("Sucess " + result)
-            res.send(result)
-            } catch (err) {
-            res.status(500)
-            res.send(`{"error": ${err}: Update for id ${req.params.id}
-            failed`);}};
+
--- routes/entertainment.js
+++ routes/entertainment.js
@@ -3,7 +3,8 @@ const entertainment_controlers= require('../controllers/entertainment');
 var router = express.Router();
 
 /* GET home page. */
-
+router.get('/detail', entertainment_controlers.entertainment_view_one_Page);
+router.get('/create', entertainment_controlers.entertainment_create_Page)
 router.get('/', entertainment_controlers.entertainment_view_all_Page);
 module.exports = router;
 
