21e0ba1534227a3045d65171151ca5b3506e0383
Sairam Muthyala <s567075@nwmissouri.edu>
2023-11-30 19:13:04 -0600

    button

--- routes/entertainment.js
+++ routes/entertainment.js
@@ -1,13 +1,20 @@
 var express = require('express');
-const entertainment_controlers= require('../controllers/entertainment');
+const entertainment_controlers = require('../controllers/entertainment');
 var router = express.Router();
-
+// A little function to check if we have an authorized user and continue on or
+// redirect to login.
+const secured = (req, res, next) => {
+    if (req.user) {
+        return next();
+    }
+    res.redirect("/login");
+}
 /* GET home page. */
 router.get('/detail', entertainment_controlers.entertainment_view_one_Page);
 router.get('/create', entertainment_controlers.entertainment_create_Page)
 router.get('/', entertainment_controlers.entertainment_view_all_Page);
 /* GET create update page */
-router.get('/update', entertainment_controlers.entertainment_update_Page);
+router.get('/update',secured, entertainment_controlers.entertainment_update_Page);
 
 /* GET delete costume page */
 router.get('/delete', entertainment_controlers.entertainment_delete_Page);
